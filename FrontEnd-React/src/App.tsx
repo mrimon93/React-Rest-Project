3c77cfffac126b2de37149d9864cc3e202c1e1ab
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2024-01-15 22:36:46 +0100

    issueFixed: Tailwindcss

--- FrontEnd-React/src/App.tsx
+++ FrontEnd-React/src/App.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
   return (
     <>
-
+      <div>
+        <button className="border border-green-500 text-green-500 hover:bg-green-500 hover:text-white px-4 py-2 rounded">
+          Welcome To the King Of Iron Fist Tournament
+        </button>
+      </div>
+      <h1 className='border border-red-600 px-2 py-2'> Hello Muzammil Kamaly</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ FrontEnd-React/src/Components/AssetForm.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const AssetForm: React.FC = () => {
+  const [value, setValue] = useState<number>(0);
+
+
+  const handleInsert = async () => {
+    const response = await apiService.insertAsset(value);
+  };
+
+
+
+
+
+  return (
+    <div>AssetForm</div>
+  )
+}
+
+
+
+
+export default AssetForm;
