73d1bb3cfc4ad3878417c0e260d03a4ed23e0754
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2024-01-15 20:37:20 +0100

    setup for backend

---
+++ BackEnd/Controllers/Controller.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BackEnd.Controllers
+{
+    [Route("[controller]")]
+    public class Controller : Controller
+    {
+        private readonly ILogger<Controller> _logger;
+
+        public Controller(ILogger<Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
