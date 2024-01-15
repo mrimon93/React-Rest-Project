26647f47d221a20eaed8c2e7c2d944d909ca940c
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2024-01-15 20:52:17 +0100

    setup for backend -net api

--- BackEnd/Controllers/Controller.cs
+++ BackEnd/Controllers/Controller.cs
@@ -18,15 +18,17 @@ namespace BackEnd.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
-        }
+    
     }
+
+    [HttpGet]
+
+    [HttpPost]
+
+
+    [HttpDelete]
+
+
+
+
 }
---
+++ Data/Data.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Data
+    {
+        public int Id {get; set;}
+        public string Title {get; set;}
+        public bool IsCompleted {get; set;}
+    }
+}
