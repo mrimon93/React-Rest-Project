29c996d74e38e1e0aeb46c5fab90c975261b217e
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2024-01-18 13:17:22 +0100

    Frontend: Potential Solution for DayDisplay.tsx

--- FrontEnd-React/src/Components/DayDisplay.tsx
+++ FrontEnd-React/src/Components/DayDisplay.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { apiService } from '../Services/apiService';
 
-const DayDisplay = () => {
+const DayDisplay: React.FC = () => {
+  const [currentDay, setCurrentDay] = useState<number>(0);
+
+  const fetchCurrentDay = async () => {
+    const result = await apiService.getCurrentDay();
+    setCurrentDay(result.day)
+  }
+
+  useEffect(() => {
+    fetchCurrentDay();
+  } [])
+
+
+
   return (
-    <div>DayDisplay</div>
+    <div>
+      <h2>Current Day</h2>
+      <p>[currentDay]</p>
+    </div>
   )
 }
 
