77974a9a1825e27204d8ea8d70974704f959c856
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2024-01-18 13:08:37 +0100

    Frontend: Potential product for 'AssetForm'

--- FrontEnd-React/src/Components/AssetForm.tsx
+++ FrontEnd-React/src/Components/AssetForm.tsx
@@ -1,17 +1,24 @@
 import React, { useState } from 'react'
+import { apiService } from '../Services/apiService';
 
 const AssetForm: React.FC = () => {
   const [value, setValue] = useState<number>(0);
 
 
-  const handleInsert = async () => {
+  const handleInsertAsset = async () => {
     const response = await apiService.insertAsset(value);
   };
 
-
-
   return (
-    <div>AssetForm</div>
+    <div>
+      <h2>AssetForm</h2>
+      <input 
+      type="number"
+      value={value}
+      onChange={(e) => setValue(Number(e.target.value))}
+      />
+      <button onClick={handleInsertAsset}>Insert Asset</button>
+      </div>
   )
 }
 
