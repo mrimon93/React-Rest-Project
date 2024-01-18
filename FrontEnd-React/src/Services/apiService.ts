5a5ee620f926c7a01e5f390742a72f86e61b45a1
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2024-01-18 13:04:40 +0100

    Frontend: setting up api-service

--- FrontEnd-React/src/Services/apiService.ts
+++ FrontEnd-React/src/Services/apiService.ts
@@ -2,13 +2,33 @@ import React from 'react'
 
 const API_BASE_URL = 'http://localhost:5000/api';
 
-export const apiService = () => {
+export const apiService = {
   
-  
-  
-    return (
-    <div>apiService</div>
-  )
-}
+  insertAsset: async (value:number) => {
+    const response = await fetch(`${API_BASE_URL}/transactions/insertAsset`,{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ value }),
+    });
+    return response.json();
+  },
 
-export default apiService
+  insertLiability: async (value: number) => {
+    const response = await fetch(`${API_BASE_URL}/transactions/insertLiability`, {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json', 
+      },
+      body: JSON.stringify({ value }), 
+    });
+
+    return response.json();
+  },
+
+  getCurrentDay: async () => {
+    const response = await fetch(`${API_BASE_URL}/transactions/getCurrentDay`);
+    return response.json()
+  },
+};
