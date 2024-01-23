31573e8da1b1b4d8155dce2345205da6db42c3b7
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2024-01-23 11:41:08 +0100

    working: Program.cs

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -7,6 +7,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:5173", "http://localhost:3000")
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials();
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
